9f91e7dddfcf1ad7215dcc7d024b312701bc1a48
krupagaliya <krupagaliya@gmail.com>
2020-05-06 13:33:01 +0530

    minor changes

--- data_preproces.py
+++ data_preproces.py
@@ -90,6 +90,10 @@ def gen_x_y(img_paths, train_val_test='train', augmentation_methods=['ori']):
             x.append(np.expand_dims(cv2.flip(x_, 1), axis=0))
             y.append(np.expand_dims(np.expand_dims(cv2.flip(y_, 1), axis=0), axis=-1))
 
+    print(len(x), len(y))
+    cv2.imshow(img_paths[0].split('/')[-1], np.squeeze(x[1], axis=0))
+    cv2.imshow(img_paths[0].split('/')[-1] + "org", np.squeeze(x[0], axis=0))
+    cv2.waitKey(100000)
     x = np.squeeze(x, axis=1)
     y = np.squeeze(y, axis=1)
     y = np.expand_dims(y, axis=-1)
@@ -98,6 +102,6 @@ def gen_x_y(img_paths, train_val_test='train', augmentation_methods=['ori']):
 
 if __name__ == '__main__':
     paths = read_txt('data/testing.txt')
-    x_train,y_train, img_paths = gen_x_y(paths[:5])
+    x_train,y_train, img_paths = gen_x_y(paths[:5], augmentation_methods=['flip', 'ori'])
     print(x_train.shape)
     print(y_train.shape)
